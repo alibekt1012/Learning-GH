3b50a36a4bde6b2ec0b74af544df5c8390f73ea2
alibekt1012 <alibekt1012@gmail.com>
2023-01-12 11:39:35 +0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Almat Alibekov on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
